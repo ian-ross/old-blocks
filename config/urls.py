1fcb2a4cea9a4266e9c343fac35e3963c777ebd6
Ian Ross <ian@skybluetrades.net>
2022-01-22 18:55:56 +0100

    Debug toolbar NOT in production!

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+from django.config import settings
 from django.contrib import admin
 from django.urls import include, path
 
@@ -5,6 +6,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('magiclink.urls', namespace='magiclink')),
     path('', include('blocks.urls', namespace='blocks')),
-    
-    path('__debug__/', include('debug_toolbar.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ]
